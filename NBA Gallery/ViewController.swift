0ca7caa9c795a3e421002ec1ea89248ccea1ab6c
TGwonton <winston96lan@gmail.com>
2017-04-26 23:00:52 -0700

    Initial Commit!

--- NBA Gallery/ViewController.swift
+++ NBA Gallery/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    let nbaPlayers = ["Damian Lillard", "Steph Curry", "Giannis Antetokounmpo", "D'angelo Russel"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return the number of rows that are supposed to be displayed on the page.
+        return nbaPlayers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // alter the row attributes for the tableView.
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nbaPlayers[indexPath.row]
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
